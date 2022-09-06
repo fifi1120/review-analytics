1b0b394a9f18813b93ece6de349a1584000fa3e5
Fiona Xiong <xinyiwanshi1996@163.com>
2022-09-07 00:06:59 +0800

    choose

--- read_reviews.py
+++ read_reviews.py
@@ -18,3 +18,9 @@ for d in list: #list里面已经加进去了所有的清单
     sum_len = sum_len + len(d) #用len可以求清单的长度/个数；这里len(清单里面的每一项)，就是求每一项的长度
 
 print ("留言的平均长度为", sum_len/len(list))
+
+new = []
+for d in list: 
+    if len(d) < 100:
+        new.append(d) 
+print ('一共有', len(new), "笔留言长度小于100") #这里print要定个写，因为 for in （for loop）其实还是有回旋的含义在里面，等它全部回旋（筛选）完了再印出来

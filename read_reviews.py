9de86539dbfa7b53081370d7641e89ad5fd3f98f
Fiona Xiong <xinyiwanshi1996@163.com>
2022-09-06 23:54:57 +0800

    calculate ave

--- read_reviews.py
+++ read_reviews.py
@@ -12,3 +12,9 @@ print ("-----------")
 print (list[1])
 print ("------------")
 print (list[2])
+
+sum_len = 0
+for d in list: #list里面已经加进去了所有的清单
+    sum_len = sum_len + len(d) #用len可以求清单的长度/个数；这里len(清单里面的每一项)，就是求每一项的长度
+
+print ("留言的平均长度为", sum_len/len(list))
